239b52be7e7dc5097de7c6a8b1602d8520321d13
zhoubin-datareachable <bin.zhou@datareachable.com>
2022-08-01 09:54:08 +0800

    test

---
+++ entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -e
+
+main() {
+    echo "CICD-START"
+
+    # 检测是否有输入TOKEN
+    if usesBoolean "${TOKEN}"; then
+        echo "::add-mask::${TOKEN}"
+        set -x
+    fi
+
+    # 构建
+    buildingConfiguration
+
+    # 发包
+    push
+}
+
+usesBoolean() {
+    [ ! -z "${1}" ] && [ "${1}" = "true" ]
+}
+
+# 构建配置文件
+buildingConfiguration() {
+    npmrc=""
+    token="//npm.pkg.github.com/:_authToken=${TOKEN}"
+    registry="@zhoubin-datareachable:registry=https://npm.pkg.github.com"
+    npmrc="${token}${registry}"
+    sed -i "1a \\${npmrc}" .npmrc
+}
+
+# 发布包
+push() {
+    npm publish
+}
+
+main
