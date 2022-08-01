411cad143545e80487d21324895cad0e4f2bb648
zhoubin-datareachable <bin.zhou@datareachable.com>
2022-08-01 11:41:08 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,6 @@ set -e
 
 main() {
     echo "CICD-START"
-    echo "${TOKEN}"
-    # 检测是否有输入TOKEN
-    if usesBoolean "${TOKEN}"; then
-        echo "::add-mask::${TOKEN}"
-        set -x
-    fi
 
     # 构建
     buildingConfiguration
@@ -23,11 +17,12 @@ usesBoolean() {
 
 # 构建配置文件
 buildingConfiguration() {
-    npmrc=""
-    custom="//npm.pkg.github.com/:_authToken=${TOKEN}"
-    registry="@zhoubin-datareachable:registry=https://npm.pkg.github.com"
-    npmrc="${registry}${custom}"
-    echo "${npmrc}" >.npmrc
+    # npmrc=""
+    # custom="//npm.pkg.github.com/:_authToken=${TOKEN}"
+    # registry="@zhoubin-datareachable:registry=https://npm.pkg.github.com"
+    # npmrc="${registry}${custom}"
+    # echo "${npmrc}" >.npmrc
+    echo "hello"
 }
 
 # 发布包
