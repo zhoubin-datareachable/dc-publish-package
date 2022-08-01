b7cb746b2c274f76ca7edc45a780b0f6ffac087d
zhoubin-datareachable <bin.zhou@datareachable.com>
2022-08-01 13:07:41 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ usesBoolean() {
 
 # 构建配置文件
 buildingConfiguration() {
-    sed -i 's/#v/1.0.5/g' package.json
+    sed -i "s/{{version}}/${TAG}/g" package.json
     # npmrc=""
     # custom="//npm.pkg.github.com/:_authToken=${TOKEN}"
     # registry="@zhoubin-datareachable:registry=https://npm.pkg.github.com"
